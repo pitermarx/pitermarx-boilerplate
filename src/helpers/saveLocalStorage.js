857164693642d794e1295e7856ec9b17b80d7336
Pedro Marques <pedrohmarques@gmail.com>
2016-01-12 23:27:45 +0000

    Update saveLocalStorage.js

--- src/helpers/saveLocalStorage.js
+++ src/helpers/saveLocalStorage.js
@@ -3,11 +3,11 @@ export const storage = typeof(localStorage) == 'undefined' ? null : localStorage
 
 // this function transforms the reducer in a "state-saving" reducer
 export default function (reducer, name){
-  if (!ls) {
+  if (!storage) {
     return reducer
   }
   // try here to get the initial state
-  const initialStateJson = ls.getItem(name)
+  const initialStateJson = storage.getItem(name)
   const initialState = initialStateJson ? JSON.parse(initialStateJson) : undefined
   
   // return a new reducer that saves the state in localStorage
@@ -16,7 +16,7 @@ export default function (reducer, name){
     const newState = reducer(state, action)
     // do nothing if state didn't change
     if (newState !== state) {
-      ls.setItem(name, JSON.stringify(newState))
+      storage.setItem(name, JSON.stringify(newState))
     }
     // return the state
     return newState
