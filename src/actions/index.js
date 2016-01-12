957a197ba3387695d3db47b44d82664b1c00a848
Pedro Marques <pedrohmarques@gmail.com>
2016-01-12 23:30:20 +0000

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 /* 
+ * Using constants with action types here and on reducers
  * This folder can have multiple action creators
  * Be sure to export them all here in the index.js file
- * Using constants with action types here and on reducers
  */
 
 import { ADD_TODO, COMPLETE_TODO, SET_URL } from '../constants'
